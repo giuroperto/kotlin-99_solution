ef594d5c79c9c047c76ce158439833ffca98a276
Giu Roperto <giu.roperto@gmail.com>
2022-04-06 17:56:51 -0300

    refactoring some exercises to receive generic type

--- src/main/kotlin/Lists.kt
+++ src/main/kotlin/Lists.kt
@@ -1,3 +1,5 @@
+import kotlin.reflect.typeOf
+
 class Lists {
     companion object {
 
@@ -6,8 +8,8 @@ class Lists {
          * Example: last(listOf(1, 1, 2, 3, 5, 8))
          * Returns 8
          */
-        public fun last(list : List<Int>) : Int {
-            return list[list.size - 1]
+        public fun last(list : List<*>) : Any {
+            return list[list.size - 1] ?: -1
         }
 
         /**
@@ -15,8 +17,8 @@ class Lists {
          * Example: penultimate(listOf(1, 1, 2, 3, 5, 8))
          * Returns 5
          */
-        public fun penultimate(list : List<Int>) : Int {
-            return list[list.size - 2]
+        public fun penultimate(list : List<*>) : Any {
+            return list[list.size - 2] ?: -1
         }
 
         /**
@@ -25,14 +27,14 @@ class Lists {
          * Example: nth(2, listOf(1, 1, 2, 3, 5, 8))
          * Returns 2
          */
-        public fun nth(index: Int, list : List<Int>) : Int {
-            var number: Int? = null;
+        public fun nth(index: Int, list : List<*>) : Any {
+            var item: Any? = null;
             try {
-                number = list[index]
+                item = list[index]
             } catch (e: ArrayIndexOutOfBoundsException) {
                 println("Index is out of bound!")
             }
-            return number ?: -1
+            return item ?: -1
         }
 
         /**
@@ -40,7 +42,7 @@ class Lists {
          * Example: length(listOf(1, 1, 2, 3, 5, 8))
          * Returns 6
          */
-        public fun length(list : List<Int>) : Int {
+        public fun length(list : List<*>) : Int {
             var numberOfElements = 0
             list.forEach { _ -> numberOfElements++; }
             return numberOfElements
@@ -79,26 +81,54 @@ class Lists {
             }
             return true;
         }
+
+        /**
+         * P07 (*) Flatten a nested list structure.
+         * Example: flatten(listOf(listOf(1, 1), 2, listOf(3, listOf(5, 8))))
+         * Returns [1, 1, 2, 3, 5, 8]
+         */
+        public fun flatten(list : List<Int>) : Boolean {
+            val newList: MutableList<Int> = mutableListOf()
+            var index = 0;
+
+            while (list[index] is List<*>) {
+
+            }
+
+            return
+        }
+
+        /**
+         * P08 (*) Eliminate consecutive duplicates of list elements.
+         * If a list contains repeated elements, they should be replaced with a single copy of the element.
+         * The order of the elements should not be changed.
+         * Example: compress("aaaabccaadeeee".toList())
+         * Returns [a, b, c, a, d, e]
+         */
+        public fun flatten(list : List<Int>) : Boolean {
+            return
+        }
+
+        /**
+         * P09 (*) Pack consecutive duplicates of list elements into sublists.
+         * If a list contains repeated elements, they should be placed in separate sublists.
+         * Example: pack("aaaabccaadeeee".toList())
+         * Returns [[a, a, a, a], [b], [c, c], [a, a], [d], [e, e, e, e]]
+         */
+        public fun flatten(list : List<Int>) : Boolean {
+            return
+        }
+
+        /**
+         * P10 (*) Run-length encoding of a list.
+         * Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+         * Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates
+         * of the element E.
+         * Example: encode("aaaabccaadeeee".toList())
+         * Returns [(4, a), (1, b), (2, c), (2, a), (1, d), (4, e)]
+         */
+        public fun flatten(list : List<Int>) : Boolean {
+            return
+        }
     }
 }
-
-//P07 (*) Flatten a nested list structure.
-//Example:
-//
-//> flatten(listOf(listOf(1, 1), 2, listOf(3, listOf(5, 8))))
-//[1, 1, 2, 3, 5, 8]
-//P08 (*) Eliminate consecutive duplicates of list elements.
-//If a list contains repeated elements, they should be replaced with a single copy of the element. The order of the elements should not be changed. Example:
-//
-//> compress("aaaabccaadeeee".toList())
-//[a, b, c, a, d, e]
-//P09 (*) Pack consecutive duplicates of list elements into sublists.
-//If a list contains repeated elements, they should be placed in separate sublists. Example:
-//
-//> pack("aaaabccaadeeee".toList())
-//[[a, a, a, a], [b], [c, c], [a, a], [d], [e, e, e, e]]
-//P10 (*) Run-length encoding of a list.
-//Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E. Example:
-//
-//> encode("aaaabccaadeeee".toList())
-//[(4, a), (1, b), (2, c), (2, a), (1, d), (4, e)]
